153b714d269df594da7b21d347d54e49571d0a97
Tarek Mostafa <102294486+TarekMostafa20@users.noreply.github.com>
2023-11-24 09:43:48 +0200

    Update Router.java

--- sync/src/Router.java
+++ sync/src/Router.java
@@ -1,15 +1,19 @@
-public class Router 
+/*
+public class Router
 {
     public int size;
     public Router(int a)
-    {
+    {                               //old ver
         size = a;
     }
     public int connections = 1;
     Semaphore loged_in = new Semaphore();
     Semaphore loged_out = new Semaphore();
-    
-    public void login()
+
+
+
+
+     public void login()
     {
         loged_in.setValue(size);
         System.out.println(size);
@@ -62,3 +66,45 @@ public class Router
         }
     }
 }
+*/
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Router {
+    private int size;
+    private int connections = 0;
+    private Queue<String> deviceQueue = new LinkedList<>(); //add queue
+    private Semaphore semaphore;
+
+
+    public Router(int a) {
+        size = a;
+        semaphore = new Semaphore(a);
+    }
+
+    public void login(String deviceName) throws InterruptedException {
+        semaphore.P();
+        deviceQueue.add(deviceName);
+        connections++;
+        System.out.println("- (" + deviceName + ") arrived");
+
+        if (connections <= size) {
+            System.out.println("- Connection " + connections + ": " + deviceName + " Occupied");
+        } else {
+            System.out.println("- " + deviceName + " arrived and waiting");
+        }
+
+        if (connections == size) {
+            while (!deviceQueue.isEmpty()) {
+                String nextDevice = deviceQueue.poll();
+                System.out.println("- Connection " + connections + ": " + nextDevice + " login");
+                Thread.sleep(1000); // Simulating online activity
+                System.out.println("- Connection " + connections + ": " + nextDevice + " Logged out");
+                connections--;
+                semaphore.V();
+            }
+        }
+    }
+}
+
+// removw logout
